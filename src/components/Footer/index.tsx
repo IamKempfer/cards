d2de224ed12c0b8c5b6bb59b188a9ad9266abcad
Victor Kempfer <victor.kempfer77@gmail.com>
2022-02-23 16:05:13 -0400

    ✨ Add footer content

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,7 @@
 import styles from "./style.module.css";
 
 export function Footer() {
-  return <footer className={styles.footer}>footer</footer>;
+  return (
+    <footer className={styles.footer}>Feito com ♥ por Victor Kempfer </footer>
+  );
 }
