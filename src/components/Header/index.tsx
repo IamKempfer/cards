9f71d275a12d2825ae13de45cdca3d8e50eb8bba
Victor Kempfer <victor.kempfer77@gmail.com>
2022-02-23 16:03:05 -0400

    ✨ Add target and styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,13 +7,25 @@ export function Header() {
     <header className={styles.header}>
       <h1>Logo</h1>
       <nav>
-        <a href="foo" target="_blank">
+        <a
+          href="https://www.facebook.com/"
+          target="_blank"
+          className={styles.facebook}
+        >
           <BsFacebook />
         </a>
-        <a href="foo" target="_blank">
+        <a
+          href="https://www.instagram.com/kmpzin/"
+          target="_blank"
+          className={styles.twitter}
+        >
           <BsTwitter />
         </a>
-        <a href="foo" target="_blank">
+        <a
+          href="https://twitter.com/kmpzin_foo"
+          target="_blank"
+          className={styles.instagram}
+        >
           <BsInstagram />
         </a>
       </nav>
